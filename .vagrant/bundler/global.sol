e3430318153c45c64896f1f2dab09de3cca1b982
vickee <vigneshsenthivel7@gmail.com>
2024-11-27 06:16:10 +0530

    Setting up Kubernetes using KubeADM

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-cachier",["= 1.2.1"]],["vagrant-faster",["= 0.2.0"]],["vagrant-hostmanager",["= 1.8.10"]],["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]]],"checksum":"b14e7f1078ec7117fa0564d4a15efeac049ec1cc3f5d2432ba570293ce45aa5b","vagrant_version":"2.4.1"}
